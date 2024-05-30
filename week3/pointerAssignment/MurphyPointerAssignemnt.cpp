a6021c4b5f09fd4cace1a22854fae9a209af7351
CamJam <cameron.e.murphy@gmail.com>
2024-05-30 18:38:55 -0400

    rent function finish

--- week3/pointerAssignment/MurphyPointerAssignemnt.cpp
+++ week3/pointerAssignment/MurphyPointerAssignemnt.cpp
@@ -4,14 +4,14 @@
 
 #include<iostream>
 #include<inputValidation.h>
+#include<sort.h>
 #include<vector>
 using namespace std;
 
 int getMenuItem();
-void enterRents(vector<float>, int);
+void enterRents(vector<float>&);
 void displayRents(vector<float>);
 void displayMemoryLocations(vector<float>);
-void selectionSort(vector<float>);
 
 
 int main(){
@@ -21,7 +21,6 @@ int main(){
 
 	do{
 		userOption = getMenuItem();
-		
 		switch (userOption){
 			case 1: enterRents(rentPrices);
 				break;
@@ -29,29 +28,36 @@ int main(){
 				break;
 			case 3: displayMemoryLocations(rentPrices);
 				break;
-			case 4: selectionSort(rentPrices);
+			case 4: //selectionSort(rentPrices);	
 				break;
 		}
-	}while(userOption != 5);
+	}while (userOption != 5);
 	return 0;
 }
 
+	//returns 5 menu options that have been validated by numberValidation function
 int getMenuItem(){
 	int userOption;
-	string errorMessage = "Please enter a valid option 1 - 5\n"; //for display incorrect cin
 	//prompt user
 	cout << "Choose an option: " << endl;
 	cout << "1.Enter rent prices\n2.Display rent prices\n3.Display memory locations\n4.Sort prices high to low\n5.Exit\n>>";
 	//call user validation function
-	userOption = numberValidation(errorMessage,1,5); 
+	userOption = userValidation("Please enter a valid option 1 - 5\n",1,5); 
 	return userOption;
 }
 
-void enterRents(vector<float> rentPrices, int size){
-	float 
-	for (int count = 0; count < size; count++){
-		cout << "Enter rent price " << count + 1;
+	//adds user input values to rentPrices vector using push, validates rent price as over 100 but under 100k, enters as many values as user needs
+void enterRents(vector<float>& rentPrices){
+	char userOption;
+	float rentPrice;
 
+	do{
+		cout << "Enter a Rent price: ";
+		rentPrice = userValidation("Please enter a valid rent price: ", 100, 100000);
+		rentPrices.push_back(rentPrice); //add value to vector
+		cout << "\nDo you want to enter another rent price? Y/N?: ";
+		userOption = userValidation("\nDo you want to enter another rent price? Y/N: ", 'Y', 'N');
+	}while (userOption != 'N');
 }
 
 void displayRents(vector<float> rentPrices){
@@ -60,8 +66,4 @@ void displayRents(vector<float> rentPrices){
 
 void displayMemoryLocations(vector<float> rentPrices){
 	cout << "display memory location" << endl;
-}
-
-void selectionSort(vector<float> rentPrices){
-	cout << "call selection sort" << endl;
 }
